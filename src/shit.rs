c263bc214b0a6d9748db3cf55a97bba9e90947d8
Boss <bossnathakorn@gmail.com>
2021-04-05 22:36:13 +0700

    add kill shit

--- src/shit.rs
+++ src/shit.rs
@@ -12,6 +12,17 @@ use ggez::input::keyboard::KeyCode;
 pub struct Shit {
     rect: Rect,
     velocity: Point2<f32>,
+    is_alive: bool
+}
+
+impl Shit {
+    pub fn kill(&mut self) {
+        self.is_alive = false;
+    }
+
+    fn hit_floor(&self, frame_h: f32) -> bool {
+        self.rect.y + self.rect.h > frame_h
+    }
 }
 
 impl Default for Shit {
@@ -19,6 +30,7 @@ impl Default for Shit {
         Self {
             rect: Rect::new(100f32, 200f32, BIRD_SIZE, BIRD_SIZE),
             velocity: Point2 { x: 0f32, y: 0f32 },
+            is_alive: true
         }
     }
 }
@@ -26,16 +38,23 @@ impl Default for Shit {
 impl EventHandler for Shit {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
         let delta = ggez::timer::delta(_ctx).as_secs_f32();
+        let (_, h) = ggez::graphics::drawable_size(_ctx);
         self.velocity.y += GRAVITY;
 
         let is_pressed = ggez::input::keyboard::is_key_pressed(_ctx, KeyCode::Space);
         let is_hold = ggez::input::keyboard::is_key_repeated(_ctx);
-        if is_pressed && !is_hold {
+        if is_pressed && !is_hold && self.is_alive {
             self.velocity.y = JUMP_FORCE;
         }
 
         self.rect.x += self.velocity.x * delta;
-        self.rect.y += self.velocity.y * delta;
+
+        if self.hit_floor(h) {
+            self.kill();
+        } else {
+            self.rect.y += self.velocity.y * delta;
+        }
+
         Ok(())
     }
 
