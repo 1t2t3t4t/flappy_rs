edfa522e08e706b286b37d7ee4f62a7712df5cc1
Boss <bossnathakorn@gmail.com>
2021-04-05 21:32:16 +0700

    impl jump

--- src/constant.rs
+++ src/constant.rs
@@ -5,3 +5,7 @@ pub mod color {
 
     pub const BLUE: Color = Color::new(0.529, 0.808, 0.922, 1.0);
 }
+
+pub mod world {
+    pub const GRAVITY: f32 = 9.8;
+}
--- src/shit.rs
+++ src/shit.rs
@@ -3,24 +3,40 @@ use ggez::event::EventHandler;
 use ggez::graphics::{Color, DrawMode, MeshBuilder};
 use ggez::graphics::mint::Point2;
 
-use crate::constant::EMPTY_DRAW_PARAM;
+use crate::constant::{EMPTY_DRAW_PARAM, world::GRAVITY};
 use crate::game_state::{GameComponent, Priority};
+use ggez::input::keyboard::KeyCode;
+
+const JUMP_FORCE: f32 = -300f32;
 
 #[derive(Debug)]
 pub struct Shit {
     position: Point2<f32>,
+    velocity: Point2<f32>
 }
 
 impl Default for Shit {
     fn default() -> Self {
         Self {
-            position: Point2 { x: 40f32, y: 40f32 },
+            position: Point2 { x: 150f32, y: 200f32 },
+            velocity: Point2 { x: 0f32, y: 0f32 }
         }
     }
 }
 
 impl EventHandler for Shit {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        let delta = ggez::timer::delta(_ctx).as_secs_f32();
+        self.velocity.y += GRAVITY;
+
+        let is_pressed = ggez::input::keyboard::is_key_pressed(_ctx, KeyCode::Space);
+        let is_hold = ggez::input::keyboard::is_key_repeated(_ctx);
+        if is_pressed && !is_hold {
+            self.velocity.y = JUMP_FORCE;
+        }
+
+        self.position.x += self.velocity.x * delta;
+        self.position.y += self.velocity.y * delta;
         Ok(())
     }
 
