a499b8b1aca46c6d77364345f521d8fd1f25360b
Boss <bossnathakorn@gmail.com>
2021-04-06 16:50:31 +0700

    refac scoreboard

--- src/score_board.rs
+++ src/score_board.rs
@@ -16,21 +16,23 @@ impl EventHandler for ScoreBoard {
 
     fn draw(&mut self, _ctx: &mut Context) -> GameResult {
         let (w, _) = ggez::graphics::drawable_size(_ctx);
-        let font = Font::default();
+        let font = Some(Font::default());
+        let scale = Some(Scale::uniform(40f32));
+        let text = format!("Score: {}", self.score);
 
         let text_border = Text::new(TextFragment {
-            text: format!("Score: {}", self.score),
-            font: Some(font),
-            scale: Some(Scale::uniform(40f32)),
+            text: text.to_owned(),
+            font,
+            scale,
             color: Some(ggez::graphics::BLACK),
         });
         let x_pos_border = w / 2f32 - (text_border.width(_ctx) as f32 / 2f32);
         ggez::graphics::draw(_ctx, &text_border, ([x_pos_border + 2f32, 0f32 + 2f32],))?;
 
         let text = Text::new(TextFragment {
-            text: format!("Score: {}", self.score),
-            font: Some(font),
-            scale: Some(Scale::uniform(40f32)),
+            text,
+            font,
+            scale,
             ..Default::default()
         });
         let x_pos = w / 2f32 - (text.width(_ctx) as f32 / 2f32);
