0b631e30201ff4505a7eb32852df0ba42b034f8d
Boss <bossnathakorn@gmail.com>
2021-07-03 01:09:28 +0700

    add copy resource build script

---
+++ build.rs
@@ -0,0 +1,19 @@
+use std::fs;
+use std::path::Path;
+
+fn copy_res_dir(target_path: String) {
+    let res_path = Path::new("resources");
+    let dir = fs::read_dir(res_path).unwrap();
+    for file in dir {
+        if let Ok(entry) = file {
+            let path = format!("./{}/resources", target_path);
+            fs::create_dir_all(&path).unwrap();
+            fs::copy(entry.path(), format!("./{}/resources/{}", target_path, entry.file_name().to_str().unwrap())).unwrap();
+        }
+    } 
+}
+
+fn main() {
+    let profile = std::env::var("PROFILE").unwrap();
+    copy_res_dir(format!("./target/{}", profile));
+}
